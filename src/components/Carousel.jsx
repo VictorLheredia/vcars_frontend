3155967621be3ccbfbb38a8b55ffd9d82a23c8e9
VictorLHeredia <victorluizheredia@gmail.com>
2022-04-20 16:36:36 -0300

    ajuste tamanho thumbnail

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,17 @@
 import "react-image-gallery/styles/css/image-gallery.css";
+import "../styles/Carousel.css";
 import ImageGallery from "react-image-gallery";
 
 export default function Carousel(carouselObject) {
-  
   const images = carouselObject.urlArray;
 
   return (
-    <ImageGallery showPlayButton={false} showBullets={true} items={images} />
+    <div id="Carousel">
+      <ImageGallery
+        showPlayButton={false}
+        showBullets={true}
+        items={images}
+      />
+    </div>
   );
 }
